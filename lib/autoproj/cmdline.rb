e706d8335665e6cd3e19524d16d5952e0e5edc07
Sylvain Joyeux <sylvain.joyeux@fieb.org.br>
2014-11-19 07:20:32 -0200

    do not chdir() in the root dir anymore
    All operations should now be done relative to root_dir anyways

--- lib/autoproj/cmdline.rb
+++ lib/autoproj/cmdline.rb
@@ -1218,7 +1218,6 @@ where 'mode' is one of:
 
             remaining_arguments = Autoproj::CmdLine.
                 parse_arguments(cmdline_arguments, false)
-            Dir.chdir(Autoproj.root_dir)
 
             Autoproj::CmdLine.update_os_dependencies = false
             Autoproj::CmdLine.initialize
